c8cae6c2b963e7c70bb3c1b907cba209d7b5c809
Phan Ngoc Cao Huy <jimmyhuy1997@gmail.com>
2019-03-31 22:30:42 +0700

    BT 1-4-2019

--- templates/ex1.py
+++ templates/ex1.py
@@ -1,26 +1,18 @@
-# import LocVer3
 from pylab import *
 from PIL import Image
 import cv2
 
-img = array(Image.open('1.jpg').convert('L'))  # convert('L'))=>trang den
+img = array(Image.open('static/chess.jpg').convert('L'))  # convert('L'))=>trang den
+img1 = Image.open('static/chess.jpg')
+img1 = array(img1)
 gray()
-img = cv2.GaussianBlur(img, (3, 3), 1.0)
-# sigma = 0.33
-# medianG = np.median(img)
-# TH_low = int(max(0, (1.0 - sigma) * medianG))
-# TH_high = int(min(255, (1.0 + sigma) * medianG))
-# edged = cv2.Canny(img, TH_low, TH_high) 
-# title('OpenCV')
-# imshow(edged)
-###############################
-# noisyImg = img
-x, y = img.shape
-
-# figure()
-title('Goc')
+print('B1')
+# img = cv2.GaussianBlur(img, (3, 3), 1.0)
+title('IMG')
 imshow(img)
-# imgout = LocVer3.Loc(noisyImg, '1', 3)
+
+
+print('B2')
 prewittx = array([[1, 0, -1], [1, 0, -1], [1, 0, -1]])
 prewitty = array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]])
 horFilter = cv2.filter2D(img, -1, prewittx)
@@ -31,72 +23,58 @@ verFilter = cv2.filter2D(img, -1, prewitty)
 # figure()
 # title('verFilter')
 # imshow(verFilter)
-Gradient = np.empty(horFilter.shape)
-Alpha = np.empty([x, y])
-
+# horFilter = np.empty([5,5])
+# verFilter = np.empty([5,5])
+# horFilter = ([5,5,5,0,1],[0,0,0,0,0],[5,5,10,5,5],[0,0,10,10,10],[30,30,30,0,0])
+# verFilter = ([5,0,0,10,30],[5,0,0,10,0],[5,0,15,10,20],[10,0,15,10,10],[10,0,15,10,0])
+s = np.zeros([2,2])
+_s = np.zeros([2,2])
+x, y = img.shape
+A = np.zeros([x,y])
+D = 1
+Wmn = 1
+s11 = 0
+s12 = 0
+s22 = 0
 for i in range(x):
     for j in range(y):
-        if horFilter[i, j] != 0:
-            Gradient[i, j] = (arctan(verFilter[i, j] / horFilter[i, j]) * 180) / pi
-        else:
-            Gradient[i, j] = 90
-        if 315 + 22.5 <= Gradient[i, j] or Gradient[i, j] < 22.5 or 135 + 22.5 <= Gradient[i, j] < 225 - 22.5:
-            Gradient[i, j] = 0
-        elif 22.5 <= Gradient[i, j] < 45 + 22.5 or 225 - 22.5 <= Gradient[i, j] < 225 + 22.5:
-            Gradient[i, j] = 45
-        elif 90 - 22.5 <= Gradient[i, j] < 90 + 22.5 or 270 - 22.5 <= Gradient[i, j] < 270 + 22.5:
-            Gradient[i, j] = 90
-        elif 135 - 22.5 <= Gradient[i, j] < 135 - 22.5 or 315 - 22.5 <= Gradient[i, j] < 315 + 22.5:
-            Gradient[i, j] = 135    
-        Alpha[i, j] = sqrt((int(horFilter[i, j]) * int(horFilter[i, j])) + (int(verFilter[i, j]) * int(verFilter[i, j])))
-        if i>0 and j>0 and i<x-1 and j<y-1:
-            if Gradient[i, j] == 0:
-                if Alpha[i+1, j] > Alpha[i, j] or Alpha[i-1, j] > Alpha[i, j]:
-                    Alpha[i, j] = 0
-            elif Gradient[i, j] == 45:
-                if Alpha[i+1, j+1] > Alpha[i, j] or Alpha[i-1, j-1] > Alpha[i, j]:
-                    Alpha[i, j] = 0
-            elif Gradient[i, j] == 90:
-                if Alpha[i, j+1] > Alpha[i, j] or Alpha[i, j-1] > Alpha[i, j]:
-                    Alpha[i, j] = 0
-            elif Gradient[i, j] == 135:
-                if Alpha[i-1, j+1] > Alpha[i, j] or Alpha[i+1, j-1] > Alpha[i, j]:
-                    Alpha[i, j] = 0
+        for m in range(i-D, i+D+1):
+            for n in range(j-D, j+D+1):
+                if m>x-1 or n >y-1:
+                    _s = np.zeros([2,2])
+                else:
+                    s11 = int(horFilter[m,n])*int(horFilter[m,n])
+                    s12 = int(horFilter[m,n])*int(verFilter[m,n])
+                    s22 = int(verFilter[m,n])*int(verFilter[m,n])
+                    _s[0,0] = s11
+                    _s[0,1] = s12
+                    _s[1,0] = s12
+                    _s[1,1] = s22
+                s = s+_s*Wmn
+        
+        A[i,j] = np.linalg.det(s) - (s[0,0]+s[1,1])*0.04
+        if A[i,j]<0:
+            A[i,j] = 0
+        s = np.zeros([2,2])
+print('B4')
+for i in range(x):
+    for j in range(y):
+        if i-1<0 or i+1>x-1 or j-1<0 or j+1>y-1:
+            continue
+        if A[i+1, j] < A[i, j] or A[i-1, j] < A[i, j]:
+            continue
+        if A[i+1, j+1] < A[i, j] or A[i-1, j-1] < A[i, j]:
+            continue
+        if A[i, j+1] < A[i, j] or A[i, j-1] < A[i, j]:
+            continue
+        if A[i-1, j+1] < A[i, j] or A[i+1, j-1] < A[i, j]:
+            continue
+        A[i, j] = 0
 
-# for i in range(1, x-1):
-#     for j in range(1, y-1):
-#         if Gradient[i, j] == 0:
-#             if Alpha[i+1, j] > Alpha[i, j] or Alpha[i-1, j] > Alpha[i, j]:
-#                 Alpha[i, j] = 0
-#         elif Gradient[i, j] == 45:
-#             if Alpha[i+1, j+1] > Alpha[i, j] or Alpha[i-1, j-1] > Alpha[i, j]:
-#                 Alpha[i, j] = 0
-#         elif Gradient[i, j] == 90:
-#             if Alpha[i, j+1] > Alpha[i, j] or Alpha[i, j-1] > Alpha[i, j]:
-#                 Alpha[i, j] = 0
-#         elif Gradient[i, j] == 135:
-#             if Alpha[i-1, j+1] > Alpha[i, j] or Alpha[i+1, j-1] > Alpha[i, j]:
-#                 Alpha[i, j] = 0
-                
-sigma = 0.66
-medianG = np.median(img)
-TH_low = int(max(0, (1.0 - sigma) * medianG))# max(x,y)
-TH_high = int(min(255, (1.0 + sigma) * medianG))
-print (TH_low, TH_high, medianG)
-a = 1
-while a:
-    for i in range(x):
-        for j in range(y):
-            if Alpha[i, j] > TH_high:
-                Alpha[i, j] = 255
-            elif Alpha[i, j] < TH_low:
-                Alpha[i, j] = 0
-            elif Alpha[i, j] > TH_low:
-                Alpha[i, j] = 255
-            else:
-                a = 0
-
+print(A)
 figure()
-title('Canny')
-imshow(Alpha)
+imshow(A)
+img1[A>0.01*A.max()]=[0,0,255]
+figure()
+imshow(img1)
 show()
