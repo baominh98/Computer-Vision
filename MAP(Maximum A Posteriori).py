ce01a42a3ff50adb61ac2c97691445985b25bd7a
Phan Ngoc Cao Huy <jimmyhuy1997@gmail.com>
2019-04-07 20:03:01 +0700

    Update 7-4-2019

---
+++ MAP(Maximum A Posteriori).py
@@ -0,0 +1,24 @@
+from pylab import *
+
+
+def Tong(N, Ak):
+    T = 0
+    for i in range(6):
+        T = N[i] + Ak[i] -1 + T
+    return T
+
+
+N = np.zeros(6)
+Y = np.zeros(6)
+for i in range(6):
+    while(1):
+        try:
+            N[i] = input("Nhap So lan tung So " + str(i+1) + ": ")
+            break
+        except:
+            print('Vui Long Nhap So')
+Ak = np.ones(6)
+T = Tong(N,Ak)
+for i in range(6):
+    Y[i] = (N[i] + Ak[i] -1)/T
+print(Y)
